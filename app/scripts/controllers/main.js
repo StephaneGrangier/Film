30a379fb3dc5b69ff8688fcf042f49ec5a0bdf6e
StephaneGrangier <stephane.grangier@IG14-GrangierS.intra.eiaj.ch>
2015-06-30 10:46:40 +0200

    utilisation du service(factory) MovieDB

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,35 @@
  * Controller of the filmApp
  */
 angular.module('filmApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.films = getListeFilms();
+  .controller('MainCtrl', function ($scope, MovieDB) {
+   
+    $scope.films = MovieDB.getListeFilms();
 
 
+    $scope.monClick = function(){
+		var titre = $scope.TitreNouveauFilm;
+		var commentaire = $scope.CommentaireNouveauFilm;
+		var nouveauFilm = {
+			titre: titre,
+			commentaire : commentaire
+		}
+
+		MovieDB.ajoutFilm(nouveauFilm);
+	};
+
+	$scope.suprElement = function(film){
+		MovieDB.suppressionFilm(film);
+		/*
+		var indice = $scope.films.indexOf(film);
+		$scope.films.splice(indice,1);	
+		
+		var filmsSerialise = JSON.stringify($scope.films);  	//serialiser
+		localStorage.setItem('listeFilms',filmsSerialise);	//ajout au LocalStorage
+	*/
+	};
+
+
+/*
     //récupération des films du localStorage, si vide l'initialise
     function getListeFilms(){
     	var listeFilmsJSON = localStorage.getItem('listeFilms') || "[]";
@@ -46,7 +71,7 @@ angular.module('filmApp')
 		localStorage.setItem('listeFilms',filmsSerialise);	//ajout au LocalStorage
 	};
 
-
+*/
 	$scope.active = true;
 	$scope.nbAffiche = 10;
   });
--- app/scripts/services/moviedb.js
+++ app/scripts/services/moviedb.js
@@ -16,14 +16,69 @@ angular.module('filmApp')
 
     var movies = [];
     
-    for(var i=0; i<10; i++){
+  /*  for(var i=0; i<10; i++){
       movies.push({titre: 'Element ' + (i+1), commentaire: '...' +i})
-    }
+    }*/
+
 
     // Public API here
     return {
       someMethod: function () {
         return movies;
-      }
-    };
-  });
+      },
+ 
+    //récupération des films du localStorage, si vide l'initialise
+    getListeFilms: function() {
+    	var listeFilmsJSON = localStorage.getItem('listeFilms') || "[]";
+    	var listeFilms = JSON.parse(listeFilmsJSON);
+    	return movies;
+    },
+
+    ajoutFilm: function(nouveauFilm) {
+    	movies.push(nouveauFilm);
+    	
+    	var filmsSerialise = JSON.stringify(movies);  		//serialiser
+		localStorage.setItem('listeFilms',filmsSerialise);	//ajout au LocalStorage
+
+    },
+
+	suppressionFilm: function(film) {
+		var indice = movies.indexOf(film);
+		movies.splice(indice,1);	
+		var filmsSerialise = JSON.stringify(movies);  //serialiser
+		localStorage.setItem('listeFilms',filmsSerialise);	//ajout au LocalStorage
+	}
+   
+   }
+
+   });
+
+/*
+	//ajout d'un film
+	$scope.monClick = function(){
+		var titre = $scope.TitreNouveauFilm;
+		var commentaire = $scope.CommentaireNouveauFilm;
+		var nouveauFilm = {
+			titre: titre,
+			commentaire : commentaire
+		};
+
+		$scope.films.push($scope.TitreNouveauFilm);
+		$scope.TitreNouveauFilm = '';
+		$scope.CommentaireNouveauFilm = '';
+		
+		var filmsSerialise = JSON.stringify($scope.films);  	//serialiser
+		localStorage.setItem('listeFilms',filmsSerialise);	//ajout au LocalStorage
+	};
+
+	//suppression d'un film
+	$scope.suprElement = function(film){
+		var indice = $scope.films.indexOf(film);
+		$scope.films.splice(indice,1);	
+		
+		var filmsSerialise = JSON.stringify($scope.films);  	//serialiser
+		localStorage.setItem('listeFilms',filmsSerialise);	//ajout au LocalStorage
+	};
+*/
+
+    
